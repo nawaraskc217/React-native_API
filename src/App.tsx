01aadcd5eef4e4ac2e769f2653dbfa7f441b13bb
Nawaras kc <nawaraskc888@gmail.com>
2025-02-27 12:51:14 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import SwitchCheez from '../components/SwitchCheez'
+
+
+export default function App() {
+  return (
+    <>
+    {/* <PokemonCard /> */}
+
+    <SwitchCheez />
+    </>
+  )
+}
+
+const styles = StyleSheet.create({})
